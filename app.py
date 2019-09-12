d49d1c1334905f1b008ec89676f451ad6e136ce6
Ethan <ethan.lv@feixitek.com>
2019-09-12 17:21:54 +0800

    Ethan 第一次提交，增加app.py datapc函数

--- app.py
+++ app.py
@@ -1,11 +1,89 @@
-from flask import Flask
+#!/usr/bin/python
+# coding:utf-8
+
+from flask import Flask,render_template
+import difflib
+import re
+import os
+import pexpect
+import sys
+
+# reload(sys)
+# sys.setdefaultencoding('utf-8')
 
 app = Flask(__name__)
 
+# def disk_view():
+#     import commands
+#     diskmount = commands.getoutput("sudo lsblk -lf ")
+#     return diskmount
+
+
+def datapc():
+    import commands
+    datapc = commands.getoutput("sudo lsblk -lf ")
+    # 正则表达式
+    re_disk_device = r'(sd[a-z]{1,2}\b)'
+    re_partition_no_fs = r'.*(sd\D{1,2}\d{1,2})'
+    re_partition_fs_not_mount = r'.*(sd\D{1,2}\d{1,2}) (\S+).*[^/]'
+    re_partition_fs_mounted = r'(sd\D{1,2}\d{1,2}) (\S+).* (/.*)'
+
+    lst_line = datapc.split('\n')
+
+    lstDisk = []
+    dicDI = {}
+
+    for line in lst_line:
+        re_disk_device_result = re.match(re_disk_device, line)
+        re_partition_no_fs_result = re.match(re_partition_no_fs, line)
+        re_partition_fs_not_mount_result = re.match(re_partition_fs_not_mount, line)
+        re_partition_fs_mounted_result = re.match(re_partition_fs_mounted, line)
+
+        if re_disk_device_result:
+            disk_device = str(re_disk_device_result.group())
+            lstDisk.append(disk_device)
+            dicDI[disk_device] = []
+            continue
+        else:
+            if re_partition_fs_mounted_result:
+                lstPartion = re_partition_fs_mounted_result.groups()
+                lstPartion = list(lstPartion)
+                lstPartion.append('0')
+                if lstDisk:
+                    dicDI[lstDisk[-1]].append(list(lstPartion))
+                continue
+            elif re_partition_fs_not_mount_result:
+                lstPartion = re_partition_fs_not_mount_result.groups()
+                lstPartion = list(lstPartion)
+                lstPartion.append('')
+                lstPartion.append('1')
+                if lstDisk:
+                    dicDI[lstDisk[-1]].append(list(lstPartion))
+                continue
+            elif re_partition_no_fs_result:
+                lstPartion = re_partition_no_fs_result.groups()
+                lstPartion = list(lstPartion)
+                lstPartion.append('')
+                lstPartion.append('')
+                lstPartion.append('2')
+                if lstDisk:
+                    dicDI[lstDisk[-1]].append(list(lstPartion))
+                continue
+    # print(lstDisk)
+    # pprint.pprint(dicDI)
+        print (dicDI)
+    return dicDI
+
+datapc()
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    print (datapc())
+    return render_template('index.html')
+
+
+
+
 
 
 if __name__ == '__main__':
